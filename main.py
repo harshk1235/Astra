c7eb5040b8548abd3d936d86ecb4df499b356c7c
Harsh Kumar <95571697+harshk1235@users.noreply.github.com>
2025-06-15 23:19:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
+#load_dotenv()
+TOKEN = os.environ.get("TOKEN")
 
 # Define intents and bot setup
 intents = discord.Intents.default()
